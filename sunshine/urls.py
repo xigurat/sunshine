de86575f425f9bf8ffb458fd7c73ef577e27685e
Eddy Ernesto del Valle Pino <edelvalle@hab.uci.cu>
2012-05-13 20:57:31 -0400

    quitando ajaxice y poniendo las apps actuales

--- sunshine/urls.py
+++ sunshine/urls.py
@@ -1,17 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from .views import Home
+
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sunshine.views.home', name='home'),
-    # url(r'^sunshine/', include('sunshine.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', Home.as_view(), name='home'),
+    url(r'^', include('library.urls')),
+    url(r'^', include('spine.urls')),
+    url(r'^', include('sajax.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
