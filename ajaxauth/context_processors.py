314e3a759c5c0b0aebec4bf26883ae9044c29d50
Eddy Ernesto del Valle Pino <edelvalle@hab.uci.cu>
2012-05-18 16:14:41 -0400

    agregando en el context processor el form de cambiar pass

--- ajaxauth/context_processors.py
+++ ajaxauth/context_processors.py
@@ -1,12 +1,14 @@
 
 from django.contrib.auth.forms import AuthenticationForm
-
+from django.contrib.auth.forms import PasswordChangeForm
 from .forms import SignupForm
 
 
 def ajaxauth(request):
     if request.user.is_authenticated():
-        context = {'change_password_form': None}
+        context = {
+            'change_password_form': PasswordChangeForm(user=request.user),
+        }
     else:
         context = {
             'login_form': AuthenticationForm(request),
