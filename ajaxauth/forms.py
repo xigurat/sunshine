9d82d899e5c2e266c56b75abe49fc1a75be5ceec
Eddy Ernesto del Valle Pino <edelvalle@hab.uci.cu>
2012-05-18 16:16:10 -0400

    haciendo que el form de registro requiera el nombre del usuario

--- ajaxauth/forms.py
+++ ajaxauth/forms.py
@@ -8,6 +8,12 @@ class SignupForm(RegistrationForm):
     first_name = forms.CharField(max_length=30, label=_('first name'))
     last_name = forms.CharField(max_length=30, label=_('last name'))
 
+    def __init__(self, *args, **kwargs):
+        super(SignupForm, self).__init__(*args, **kwargs)
+        # Put first_name and last_name as the first fields in the form
+        for field in ['last_name', 'first_name']:
+            self.fields.insert(0, field, self.fields.pop(field))
+
     def save(self):
         user = super(SignupForm, self).save()
         user.first_name = self.cleaned_data['first_name']
